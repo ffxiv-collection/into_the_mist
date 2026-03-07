560f4d153b7a9c9adbc47db84ac39d993710dd5e
Maki <maki_22@hotmail.fr>
2026-03-07 13:50:19 +0100

    chore: delete numerous scratch SQL scripts and related development files and update .gitignore.

--- fix_cost_schema.sql
+++
@@ -1,3 +0,0 @@
--- Change cost column from Integer to Numeric to support prices like 5.99
-ALTER TABLE public.minion_sources 
-ALTER COLUMN cost TYPE numeric(10, 2);
--- update_minions_tooltip.sql
+++
@@ -1,7 +0,0 @@
--- Add a text column for the minion's in-game tooltip/quote
-alter table public.minions add column if not exists tooltip text;
-
--- Example:
--- update public.minions 
--- set tooltip = 'Il ne faut pas se fier à son air innocent...'
--- where name ilike '%Aerith%';
